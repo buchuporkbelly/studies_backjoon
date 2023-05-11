aa7f9a00615fb985373cc6ac03988c4ba61c70ef
buchuporkbelly <103941953+buchuporkbelly@users.noreply.github.com>
2023-05-11 14:45:57 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17620 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 자동 생성된 메소드 스텁
+		Scanner sc = new Scanner(System.in);
+		
+		char ch = sc.next().charAt(0);
+		int a = ch;
+		
+		System.out.print(a);
+		
+		sc.close();
+		
+	}
+
+}
