bdbce3a3ac2b7c4384b55ec4e1937a2c1ebfbc60
buchuporkbelly <103941953+buchuporkbelly@users.noreply.github.com>
2023-05-16 00:58:46 +0900

    [Bronze V] Title: 고양이, Time: 124 ms, Memory: 13900 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,10 @@
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 자동 생성된 메소드 스텁
+		System.out.println("\\"+"    "+"/"+"\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" "+"\\"+"(__)|");
+	}
+}
